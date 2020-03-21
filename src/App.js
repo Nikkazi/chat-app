055460889d1ad3bcb1e400f4df53b0e31523be51
Nikkazi <nickkazi@hotmail.com>
2020-03-21 15:59:24 +0000

    React3

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import './App.css';
 import Contact from './components/Contact';
+import users from './components/ContactList';
 
 function App() {
+  const ContactLists = users.map(item => {
+    return <Contact user={item} />
+  })
   return (
     <div className="App">
-      <Contact
-        nameavatar="https://randomuser.me/api/portraits/women/92.jpg"
-        firstName="Natalie"
-        lastName="Potato"
-        age={307}
-        status={true}
-      />
-      <Contact
-        nameavatar="https://randomuser.me/api/portraits/women/58.jpg"
-        firstName="Tamara"
-        lastName="Noodle"
-        age={508}
-        status={false}
-      />
-      <Contact
-        nameavatar="https://randomuser.me/api/portraits/men/30.jpg"
-        firstName="Paul"
-        lastName="Ramen"
-        age={100}
-        status={true}
-      />
+      {ContactLists}
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,12 +5,11 @@ import PropTypes from 'prop-types';
 const Contact = (props) => {
     return (
         <div className="Contact">
-            <img src={props.nameavatar} alt={props.name} />
+            <img src={props.user.avatar} alt={props.user.name} />
             <div className="Contact-info">
-                <h2>{props.firstName + " " + props.lastName}</h2>
-                <h2>Age:{props.age}</h2>
+                <h2>{props.user.name}</h2>
                 <div className="status">
-                    <p className={props.status ? 'status-online' : 'status-offline'}>{props.status ? 'online' : 'offline'}</p>
+                    <p className={props.user.online ? 'status-online' : 'status-offline'}>{props.user.online ? 'online' : 'offline'}</p>
                 </div>
             </div>
         </div >
